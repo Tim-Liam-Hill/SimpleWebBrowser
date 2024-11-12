7e9e6df340a1588cbf8151846bb4db3c11ddadbf
tim <u17112592@tuks.co.za>
2024-11-12 19:06:14 +0200

    Implemented superscript

--- python/src/Layout.py
+++ python/src/Layout.py
@@ -2,13 +2,14 @@ import sys
 import tkinter
 import tkinter.font
 from URL import URL, Text, lex
+import math
 import logging
 logger = logging.getLogger(__name__)
 
 DEFAULT_FONT_SIZE = 16 #TODO: dedicated text class 
 HSTEP, VSTEP = 13, 18
 DEFAULT_LEADING = 1.25 #do we need different leadings? TODO: later on might get this from CSS.
-DEFAULT_FONT_FAMILY = ''
+DEFAULT_FONT_FAMILY = "Segoe UI"
 FONTS = {}
 SELF_CLOSING_TAGS = [
     "area", "base", "br", "col", "embed", "hr", "img", "input",
@@ -16,11 +17,11 @@ SELF_CLOSING_TAGS = [
 ]
 
 #TODO: support more fonts 
-def get_font(size, weight, style):
+def get_font(size, weight, style, family):
     key = (size, weight, style)
     if key not in FONTS:
-        font = tkinter.font.Font(size=size, weight=weight,
-            slant=style)
+        font = tkinter.font.Font(size=math.floor(size), weight=weight,
+            slant=style, family=family)
         label = tkinter.Label(font=font)
         FONTS[key] = (font, label)
     return FONTS[key][0]
@@ -36,6 +37,8 @@ class Layout:
         self.weight = "normal"
         self.style = "roman"
         self.leading = DEFAULT_LEADING
+        self.superscript = False
+        self.family = DEFAULT_FONT_FAMILY
         self.activeTags = []
         
         #TODO: pre formatted code (after html parser I guess?)
@@ -50,34 +53,30 @@ class Layout:
             for word in tok.text.split():
                 self.word(word, width)
         else: 
-            if tok.tag in SELF_CLOSING_TAGS:
-                pass 
-            elif tok.tag[0] == '/':
-                self.activeTags.remove(tok.tag[1:])
-            elif not tok.tag in  self.activeTags:
-                self.activeTags.append(tok.tag)
-            #self.handleTag(tok.tag)
+            #TODO: this will need a big rework once dom is implemented. 
+            #I want to have some sort of decorator pattern where we 
+            #Setup defaults and then have decorators that edit the defaults
+            #as we go down. 
+            self.handleTag(tok.tag)
 
     def word(self, word, width):
-        font = get_font(self.fontSize, self.weight, self.style)
-        
+
+        font = get_font(self.fontSize, self.weight, self.style, self.family)
+
         w = font.measure(word)
         if self.cursor_x + w >= width - HSTEP:
             self.flush()
-        self.line.append((self.cursor_x, word, font, list(self.activeTags)))
+        self.line.append((self.cursor_x, word, font, self.superscript))
         self.cursor_x += w + font.measure(" ")
 
     def flush(self):
         logging.info('Flushing current line')
         if not self.line: return 
-        metrics = [font.metrics() for x, word, font, tags in self.line]
+        metrics = [font.metrics() for x, word, font, isSuperscript in self.line]
         max_ascent = max([metric["ascent"] for metric in metrics])
         baseline = self.cursor_y +  self.leading * max_ascent
-        for x, word, font, tags in self.line:
-            logging.info("Word: %s", word)
-            logging.debug("Tags: %s", tags)
-            logging.debug("")
-            y = baseline - font.metrics("ascent")
+        for x, word, font, isSuperscript in self.line:
+            y = baseline - font.metrics("ascent") if not isSuperscript else baseline - 1.8*font.metrics("ascent")
             self.display_list.append((x, y, word, font))
         max_descent = max([metric["descent"] for metric in metrics])
         self.cursor_y = baseline + 1.25 * max_descent
@@ -101,6 +100,14 @@ class Layout:
             self.fontSize += 4
         elif tag == "/big":
             self.fontSize -=4
+        elif tag == "sup":
+            self.fontSize /=2
+            self.superscript = True 
+            #self.family = "Helvetica"
+        elif tag == "/sup":
+            self.fontSize *=2
+            self.superscript = False
+            #self.family = "Segoe UI"
         elif tag == "br":
             self.flush()
             self.cursor_y += VSTEP
