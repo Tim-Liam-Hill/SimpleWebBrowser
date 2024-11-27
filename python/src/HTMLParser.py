5335d6fdda81c8cfc83a977ed4dfeda302bd6eff
tim <u17112592@tuks.co.za>
2024-11-27 18:15:08 +0200

    Added final implicit_tags function. Might do one more exercise before moving on

--- python/src/HTMLParser.py
+++ python/src/HTMLParser.py
@@ -8,6 +8,9 @@ SELF_CLOSING_TAGS = [
     "link", "meta", "param", "source", "track", "wbr",
 ]
 
+#TODO: because I was already using the textbook's 'add_tag' and 'add_text' methods
+# the DFA has the weird behaviour of ignoring some chars/checking for trailing tags. 
+#changing the aformentioned methods would mean we wouldn't need to do this (I think).
 #if a state has an 'accept' flag, we don't consume current char and instead
 #push the type of element determined by the accept flag, then transition as normal
 #if state has a 'trailing_tag' flag, the following happens when accept:
@@ -62,7 +65,6 @@ DFA = {
         "script_close6":{"p":{"next":"script_close7"},"**":{"next":"script_body"}},
         "script_close7":{"t":{"next":"script_close8"},"**":{"next":"script_body"}},
         "script_close8":{">":{"accept":"data","next":"data", "trailing_tag":"script"},"**":{"next":"script_body"}},
-
     }
 }
 
@@ -96,6 +98,10 @@ class Element:
 #TODO: mayhaps do this as a separate project at somepoint? 
 #TODO: There is definitely a better/more elegant way to implement a lexer
 class HTMLParser:
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
     def __init__(self, body):
         self.body = body
         self.tokens = []
@@ -104,6 +110,7 @@ class HTMLParser:
     #better version of parse that uses a DFA (albeit a simple one)
     #it doesn't freak out when encountering quotes inside tag bodies (nor does it freak out if there are closing tags in those quotes)
     def parse(self,viewSource = False):
+        logger.info("Tokenizing tree")
         if viewSource: #TODO: format the text with newlines and such. Maybe just use a subclass of HTMLParser
             return Text(self.body, None) #TODO: check if we need anything else for this, like a div parent
 
@@ -112,7 +119,6 @@ class HTMLParser:
         i = 0
 
         for i in self.body:
-            logger.debug("In state %s: ", state)
             if i in DFA["states"][state]:
                 if "accept" in DFA["states"][state][i] and DFA["states"][state][i]["accept"] == "tag":
                     self.add_tag(text)
@@ -144,67 +150,10 @@ class HTMLParser:
             self.add_text(text)
         return self.finish()
 
-    #could be more elegant
-    #TODO: support <pre> here with another switch case (maybe even code)
-
-    #I should really just be writing DFAs for this,but its a lot more work than what I think I need right now
-    #TODO: seriously need to replace this with a DFA and stop being lazy. 
-    def parse_old(self, viewSource = False):
-
-        if viewSource:
-            return Text(self.body, None) #TODO: check if we need anything else for this, like a div parent
-
-        text = ""
-        in_tag = False
-        i = 0
-        while i < len(self.body):
-            if self.body[i] == "<": 
-                in_tag = True
-                if text: self.add_text(text)
-                text = ""
-            elif self.body[i] == ">":
-                in_tag = False
-                self.add_tag(text)
-                #we can be sneaky here
-                #before we clear text, check what the start of the tag  is
-                #if it is script/style, then we jump to a different function 
-                #to extract their body, then just return i when done
-                #TODO: any other tags here to support? 
-                tag = text.split(" ")[0]
-                if tag in ["script", "style"]:
-                    i, text = self.parseScriptStyle(i + 1, tag) #returns the index of the last char processed (so we don't double iterate)
-                else: text = ""
-                
-            else:
-                text += self.body[i]
-            i+=1
-
-        if not in_tag and text:
-            self.add_text(text)
-        return self.finish()
-
-    #NBNBNBNB if for whatever reason a script has a string with contents "...</script..."
-    #then that will cause an issue. This shouldn't realistically be the case however (unless that site is doing
-    #something weird or shady)
-    def parseScriptStyle(self, i, tag):
-        logger.debug("Lexing a " + tag + " tag")
-        logger.debug("end condition")
-        logger.debug(tag)
-        text = ""
-        #TODO: test this: script at end of document. 
-        while i < len(self.body):
-            if i < len(self.body) - len(tag) - 3 and self.body[i:i+2+len(tag)] == "</" + tag :
-                return i -1, text
-            text += self.body[i]
-            i += 1
-        
-        return i, text
-
-
     def add_text(self, text):
         if text.isspace() or len(text) == 0: return
+        self.implicit_tags(None)
 
-        logger.debug("Adding text: %s", text)
         AMP_REMAPS = { #TODO: there is almost certainly a better way of doing this that is more performant (eh)
             "&quot;": "\"", #it would use a DFA that iterates through and replaces as strings are encountered
             "&copy;":"©",   #actually somewhat trivial to implement, will get to it when the need for performance arises
@@ -226,10 +175,11 @@ class HTMLParser:
     def add_tag(self, tag):
         
         if tag.startswith("!") or tag=="": return #we realistically shouldn't encounter empty tags, and technically I don't think we need to remove them 
+        self.implicit_tags(tag)
         logger.debug("Adding tag: %s", tag)       
 
         if tag.startswith("/"):
-            if len(self.unfinished) == 1: return
+            #if len(self.unfinished) == 1: return #TODO: why was this here? is it needed? hmmmmmmmm.....
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
             parent.children.append(node)
@@ -245,35 +195,31 @@ class HTMLParser:
             self.unfinished.append(node)
 
     def finish(self):
+        if not self.unfinished:
+            self.implicit_tags(None)
+    
         while len(self.unfinished) > 1:
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
             parent.children.append(node)
-        print_tree(self.unfinished[0])
         return self.unfinished.pop()
     
-    #TODO: expand on this so that later we can do a bit more css and such. 
-    #basically only split on the first space to get tag. For the rest of the body
-    #we need to iterate through and make sure that we aren't splitting on a space that is in
-    #between quotes (eg: imagine you have class="dark flex background-grey". Splitting this by spaces
-    #would be incorrect)
-    def get_attributes_old(self,text):
-        
-        parts = text.split()
-        tag = parts[0].casefold()
-        attributes = {}
-        for attrpair in parts[1:]:
-            if "=" in attrpair:
-                key, value = attrpair.split("=", 1)
-                if len(value) > 2 and value[0] in ["'", "\""]:
-                    value = value[1:-1]
-                attributes[key.casefold()] = value
+    def implicit_tags(self, tag):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
             else:
-                attributes[attrpair.casefold()] = ""
-        return tag, attributes
+                break
+            
     
     #obtuse but it works 
-    #TODO: actually test it more
+    #TODO: actually test it more but yeah, for now let's just assume it works :3
     def get_attributes(self,text):
         text = text.lstrip()
         parts = text.split()
@@ -320,17 +266,6 @@ def print_tree(node, indent=0):
 
 if __name__ == "__main__":
 
-    html = """<!Doctype html>
-<html>
-<body>
-<div id="meow">
-I am some <h1>Text</h1>
-<img src='http://google.com' style="meow:blue" />
-</div>
-</body>
-
-</html>
-"""
     logging.basicConfig(level=logging.DEBUG)
     url = URL()
     content = url.request("https://browser.engineering/html.html")
