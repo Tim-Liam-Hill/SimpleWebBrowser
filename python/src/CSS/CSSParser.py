06fb626781baae63f25b83673f87f9cbf59f1192
tim <u17112592@tuks.co.za>
2025-01-10 08:11:13 +0200

    Oops, forgot some files

---
+++ python/src/CSS/CSSParser.py
@@ -0,0 +1,13 @@
+import logging
+logger = logging.getLogger(__name__)
+
+class CSSParser:
+    def __init__(self, s):
+        self.s = s
+        self.i = 0
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
